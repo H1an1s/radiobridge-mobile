9e0bdd0a8f9df72bf7ec484e446e1a68e1f57226
H1an1s <Danylo.Danylov@Teledyne.com>
2026-07-22 06:44:45 +0300

    Require ntfy topic from Bark link

--- config.js
+++ config.js
@@ -3,5 +3,5 @@ window.RADIOBRIDGE_CONFIG = Object.freeze({
   allowedNtfyServers: ["https://ntfy.sh"],
   ntfyTopic: "rb_mobile_f4c7e91d6a3b8f02e5d1749ac91b2e73",
   requestTimeoutMs: 12000,
-  appVersion: "1.0.0"
+  appVersion: "1.0.1"
 });
